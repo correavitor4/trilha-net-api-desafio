60ece0413aa7e9120552e53ff0339da97cb85f8e
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2022-10-10 16:47:46 -0300

    Inserção de classe e método estáticos para criar automaticamente um migration se necessário

---
+++ Services/DatabaseManagementService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrilhaApiDesafio.Context;
+
+namespace TrilhaApiDesafio.Services;
+
+public static class DatabaseManagementService
+{
+    public static void MigrationInitialization(this IApplicationBuilder app)
+    {
+        using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+        {
+            var context = serviceScope.ServiceProvider.GetRequiredService<OrganizadorContext>();
+            context.Database.Migrate();
+        }
+    }   
+}
